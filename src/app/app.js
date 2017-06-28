ccf55f7595062b25a873c10163b0cac8627a4ee0
Jeremiah Sandoval <Jeremiah Sandoval>
2017-06-29 01:37:47 +0800

    update app and index

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,36 @@
 var React = require('react');
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Switch, IndexRoute } from 'react-router-dom';
 import FriendsList from './containers/friends/friendslist';
 import AddFriend from './containers/friends/addfriend';
+import UpdateFriend from './containers/friends/updatefriend';
+
+var About = React.createClass({
+  render() {
+    return (
+      <h1>About Page</h1>
+    );
+  }
+})
+
+var Contact = React.createClass({
+  render() {
+    return (
+      <h1>Contact Page</h1>
+    );
+  }
+})
 
 var App = React.createClass({
   render() {
     return(
       <Router>
         <Switch>
-          <Route path="/" component={FriendsList}/>
+          <Route path="/home" component={FriendsList}/>
           <Route path="/add" component={AddFriend}/>
+          <Route path="/update/:friendId" component={UpdateFriend}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact}/>
         </Switch>
       </Router>
     );
